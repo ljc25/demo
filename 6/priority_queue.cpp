000d41cc59a7757f3c630eab9a2226a7b2353f06
ljc25 <1191753703@qq.com>
2020-01-21 14:28:21 +0800

    优先级队列

--- 6/priority_queue.cpp
+++ 6/priority_queue.cpp
@@ -1,27 +1,27 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-priority_queue<int,vector<int>,greater<int> > que;
 
-priority_queue<int> a; //默认降序
-//等同于 priority_queue<int, vector<int>, less<int> > a;
+priority_queue<int,vector<int>,greater<int> > que;//定义优先队列，至于为何这样定义，上网查查。。。
+
+priority_queue<int> c; //默认降序
+//等同于 priority_queue<int, vector<int>, less<int> > c;
 priority_queue<string> b;
 
-const int N=100001;
-int a[N];
+int a[1000001];
 int n;
 int main()
 {
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     {
-        scanf("%d",&a[i]);
+        cin>>a[i];
         que.push(a[i]);//边读入便放入队列
     }
     while(!que.empty())
     {
-        printf("%d ",que.top());//直接输出
+        cout<< que.top() <<" ";
         que.pop();
     }
-    return 0;//完美！
+    return 0;
 }
