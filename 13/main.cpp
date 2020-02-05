56d1da33d6a578ac297e718e84ff725dd5cacdb7
ljc <1191753703@qq.com>
2020-02-05 18:00:05 +0800

    栈的简单应用

---
+++ 13/main.cpp
@@ -0,0 +1,32 @@
+/* P1739 */
+#include <bits/stdc++.h>
+using namespace std;
+
+char str[256];
+stack<char> p;
+
+int main()
+{
+    gets(str);
+    for (int i = 0; str[i] != '@'; i++)
+    {
+        if (str[i] == '(')
+            p.push(str[i]);
+        if (str[i] == ')' && (!p.empty()))
+        {
+            p.pop();
+            continue;
+        }
+
+        if (str[i] == ')' && p.empty())
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+    if (p.empty())
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
