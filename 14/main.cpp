4daac44f6410024a64cffc55089081e05431a3c9
ljc <1191753703@qq.com>
2020-02-06 19:17:59 +0800

    卡特兰数

--- 14/main.cpp
+++ 14/main.cpp
@@ -28,24 +28,3 @@ int main()
     cout << f[n];
     return 0;
 }
-
-//  解法二 : dfs递归
-#include <bits/stdc++.h>
-using namespace std;
-
-int n;
-
-int f[20][20];
-int dfs(int x, int y){   //x是操作数队列  y是栈中数
-    if(f[x][y]!=0) return f[x][y];
-    if(x==0) return 1;
-    if(y>0) f[x][y]+=dfs(x,y-1);
-    f[x][y]+=dfs(x-1,y+1);
-    return f[x][y];
-}
-int main()
-{
-    cin >> n;
-    cout << dfs(n,0) << endl;
-    return 0;
-}
